2003efaef583573585dfe810cb01eb40146ddc6c
Nilesh Modi <nilesh.modi@talentica.com>
2025-12-05 23:27:00 +0530

    Initial project setup - Spring Boot with all dependencies

---
+++ src/main/java/com/solarvalley/webapp/SolarValleyWebappApplication.java
@@ -0,0 +1,13 @@
+package com.solarvalley.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarValleyWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarValleyWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solarvalley/webapp/SolarValleyWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solarvalley.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarValleyWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
